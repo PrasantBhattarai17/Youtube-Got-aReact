e14978e857b4ddb87a751eab9c16799f7361a6e8
notPrasantesp17 <punkersyt@gmail.com>
2024-07-18 00:36:16 +0545

    continued.......

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,7 +2,33 @@ import React from 'react';
 
 const Sidebar = () => {
   return (
-    <div>Sidebar</div>
+    <div className='w-1/12 text-center'>
+      <div className='w-full shadow-lg h-22'>
+      <h1></h1>
+      <ul className=''>
+        <li className='font-bold'>Home</li>
+        <li className='font-bold'>Shorts</li>
+        <li className='font-bold'>Videos</li>
+      </ul>
+      </div >
+      <div className='w-full shadow-lg h-40 mt-3'>
+      <h1 className='font-bold'>Subscriptions</h1>
+      <ul>
+        <li>Gaming</li>
+        <li>Music</li>
+        <li>Movies</li>
+      </ul>
+      </div>
+      <div className='w-full shadow-lg h-40 mt-3'>
+      <h1 className='font-bold'>Explore</h1>
+      <ul>
+        <li>Trending</li>
+        <li>Music</li>
+        <li>Gaming</li>
+        <li>Sports</li>
+      </ul>
+      </div>
+    </div>
   )
 }
 
